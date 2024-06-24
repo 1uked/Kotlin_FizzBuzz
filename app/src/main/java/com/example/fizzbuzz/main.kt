2342829080430287d88ca15091ceead7ec319780
Luke Edwards <cm22683@bristol.ac.uk>
2024-06-24 17:21:07 +0100

    removed Print Function

--- app/src/main/java/com/example/fizzbuzz/main.kt
+++ app/src/main/java/com/example/fizzbuzz/main.kt
@@ -1,11 +1,10 @@
 package com.example.fizzbuzz
 
 fun main(){
-    // TODO: Implement Recursive
+    // TODO: Implement Recursive + Validation
     print("Enter an Upper Limit: ")
     val stringInput = readLine()!!
 
-    // TODO: Select Rules
     val validRules = arrayOf("3","5","7","11","13","17")
     var curRules = emptyArray<String>()
 
@@ -19,13 +18,19 @@ fun main(){
 
     try {
         for (i in 1..stringInput.toInt()) {
-            checkFizzBuzz(i,curRules)
+            println(checkFizzBuzz(i,curRules))
         }
     } catch (e: NumberFormatException) { }
 }
 
-fun checkFizzBuzz(x: Int, Rules: Array<String>) {
+fun checkFizzBuzz(x: Int, Rules: Array<String>) : String {
     var output = emptyArray<String>()
+
+    if (x % 11 == 0 && "11" in Rules) {
+        return if (x % 13 == 0 && "13" in Rules) { "FezzBong" }
+        else { "Bong" }
+    }
+
 
     if (x % 3 == 0 && "3" in Rules) { output += "Fizz" }
 
@@ -35,20 +40,9 @@ fun checkFizzBuzz(x: Int, Rules: Array<String>) {
 
     if (x % 7 == 0 && "7" in Rules) { output += "Bang" }
 
-    if (x % 11 == 0 && "11" in Rules) {
-        if (x % 13 == 0 && "13" in Rules) {
-            output = arrayOf("Fezz","Bong")
-        } else { output = arrayOf("Bong") }
-    }
-
     if (x % 17 == 0 && "17" in Rules) { output.reverse() }
-    printOutput(x, output)
-}
 
-fun printOutput(x : Int, y : Array<String>){
-    if (y.size != 0) {
-        println(y.joinToString(separator = ""))
-    } else {
-        println(x)
-    }
+    return if (output.isNotEmpty()) {
+        output.joinToString(separator = "")
+    } else { x.toString() }
 }
