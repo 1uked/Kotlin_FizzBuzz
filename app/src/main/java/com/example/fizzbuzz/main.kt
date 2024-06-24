a46d7429129a7d2178acdd911f04224b9c7b503b
Luke Edwards <cm22683@bristol.ac.uk>
2024-06-24 15:47:25 +0100

    Part 1 - Not Optimised

--- app/src/main/java/com/example/fizzbuzz/main.kt
+++ app/src/main/java/com/example/fizzbuzz/main.kt
@@ -2,4 +2,21 @@ package com.example.fizzbuzz
 
 fun main(){
     print("Hello World!")
+
+    for (i in 1..100) {
+        checkFizzBuzz(i)
+    }
+
+}
+
+fun checkFizzBuzz(x: Int) {
+    if (x % 3 == 0 && x % 5 == 0) {
+        println("FizzBuzz")
+    } else if (x % 3 == 0) {
+        println("Fizz")
+    } else if (x % 5 == 0) {
+        println("Buzz")
+    } else {
+        println(x)
+    }
 }
